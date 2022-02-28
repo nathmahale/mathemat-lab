3a6d54134783ae1a6579e2bdc66a9f9d38aa82c4
nathmahale <16042612+nathmahale@users.noreply.github.com>
2022-02-28 19:05:28 +1100

    added test alises

--- aliases.sh
+++ aliases.sh
@@ -7,3 +7,6 @@ alias dsv="docker system df -v"
 alias di="docker images -a"
 alias dp="docker ps -a"
 alias de="docker exec -it $*"
+alias dlgn='docker exec -it b86 bash'
+alias dlgn1='docker exec -it db8 /bin/sh'
+alias dlgnr='docker exec -it -u root b86 bash'
