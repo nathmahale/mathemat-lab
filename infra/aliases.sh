a75b030aaea145381c224fac36f09718c3a5a867
nathmahale <16042612+nathmahale@users.noreply.github.com>
2022-04-08 00:18:59 +1000

    shuffled files

--- infra/aliases.sh
+++ infra/aliases.sh
@@ -1,13 +1,10 @@
 alias dsize="docker history --human --format '{{.CreatedBy}}: {{.Size}}' $*"
 alias dst="docker stop $*"
-alias dr="docker rm $*"
+alias drc="docker rm $*"
 alias dri="docker rmi $*"
 alias ds="docker system df"
 alias dsv="docker system df -v"
-alias di="docker images -a"
+alias dil="docker images -a"
 alias dp="docker ps -a"
-alias de="docker exec -it $*"
-alias dlgn='docker exec -it b86 bash'
-alias dlgn1='docker exec -it db8 /bin/sh'
-alias dlgnr='docker exec -it -u root b86 bash'
+alias de="docker run -it $*"
 
--- infra/prep.sh
+++ infra/prep.sh
@@ -12,21 +12,22 @@ function create_dockerfile_jenkins() {
   cat >Dockerfile <<EOF
 FROM jenkins/jenkins:latest
 USER root
-RUN apt-get update && apt-get install -y lsb-release
-RUN curl -fsSLo /usr/share/keyrings/docker-archive-keyring.asc \
-  https://download.docker.com/linux/ubuntu/gpg
-RUN echo "deb [arch=$(dpkg --print-architecture) \
+RUN apt-get update && \
+  apt-get install -y lsb-release && \
+  curl -fsSLo /usr/share/keyrings/docker-archive-keyring.asc \
+  https://download.docker.com/linux/ubuntu/gpg && \
+  echo "deb [arch=$(dpkg --print-architecture) \
   signed-by=/usr/share/keyrings/docker-archive-keyring.asc] \
   https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list
-RUN apt-get update && apt-get install -y docker-ce-cli
+  $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list && \
+  apt-get update && apt-get install -y docker-ce-cli
 USER jenkins
 EOF
 
 }
 
 function build_docker_jenkins() {
-  docker build -t jenkins:${VERSION} .
+  docker build -t jenkins:${IMAGE_TAG_VERSION} .
 
 }
 
@@ -53,10 +54,9 @@ function create_docker_dind_container() {
 
 }
 
-VERSION=$1
-create_docker_dependancies
+IMAGE_TAG_VERSION=$1
+# create_docker_dependancies
 create_dockerfile_jenkins
-build_docker_jenkins
-create_docker_dind_container
-create_jenkins_docker_container
-
+# build_docker_jenkins
+# create_docker_dind_container
+# create_jenkins_docker_container
